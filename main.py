e20375789dd643b0c6d52fb30dfb8c40ea455f87
Gil <gil@Baileys-MacBook-Pro-2.local>
2025-03-07 15:40:16 -0500

    load dotenv

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
 import pdfplumber
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 PDF_PATH = "Palantir Q4 2024 Business Update.pdf"
 
@@ -12,9 +19,23 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
+def chunk_text(text, chunk_size=1000, chunk_overlap=100):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, 
+        chunk_overlap=chunk_overlap,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
 
 
 if __name__ == "__main__":
     extracted_text = extract_text_from_pdf(PDF_PATH)
-    print(f"Extracted {len(extracted_text.split())} words from the PDF.\n")
-    print(extracted_text[:1000]) 
+    
+    text_chunks = chunk_text(extracted_text)
+
+    print(f"Total Chunks Created: {len(text_chunks)}")
+    print("\nSample Chunk:\n", text_chunks[0])
+
